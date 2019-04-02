d8ba19efc040b743af6fcd79917b8e1f77c2e9d4
Aidan Jennings <anjennings@wisc.edu>
2019-04-02 17:12:08 -0500

    Update Polygon.java

--- Polygon.java
+++ Polygon.java
@@ -3,22 +3,49 @@ import java.util.ArrayList;
 
 public class Polygon {
 
-    public Point[] points = {null, null, null};
     public Line[] lines = {null, null};
-    public Color color;
+    public Point[] points = {null, null, null};
+    public Color color = null;
     
     public static ArrayList<Polygon> globalPolygons = new ArrayList<Polygon>();
     
-    public Polygon(Point a, Point b, Point c) {
+    public Polygon(Point a, Point b, Point c, Color col) {
       
-      this.points[0] = a;
-      this.points[1] = b;
-      this.points[2] = c;
+      lines[0] = new Line(a,b);
+      lines[1] = new Line(b,c);
+      color = col;
       
       globalPolygons.add(this);
       
     }
     
-    
+public Polygon(Line a, Line b, Color col) throws Exception {
+      
+  //This is a terrible way of doing this, think of a better one!
+      if(Line.polygonCompatible(a,b)) {
+        
+        lines[0] = a;
+        lines[1] = b;
+        color = col;
+        
+        if(Point.comparePoints(a.getA(), b.getA()) | Point.comparePoints(a.getB(), b.getA())) {
+          points[0] = a.getA();
+          points[1] = a.getB();
+          points[2] = b.getB();
+        }else if(Point.comparePoints(a.getA(), b.getB()) | Point.comparePoints(a.getB(), b.getB())) {
+          points[0] = a.getA();
+          points[1] = a.getB();
+          points[2] = b.getA();
+        }
+        
+      }else {
+        throw new Exception("Polygon Constructor : THESE TWO LINES DO NOT SHARE A COMMON POINT!");
+      }
+  
+      
+      
+      globalPolygons.add(this);
+      
+    }
   
 }
